fbe4145218d1f5d2fb905a17ecee6f2c24fd867a
Marek Lesko <mlesko100@msn.com>
2020-10-05 16:14:57 +0200

    WIP angular parser

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,12 @@
 import process from 'process';
 import jsonfile from 'jsonfile';
 import { ProcessList } from './processes/process-list';
+import fs from 'fs';
+import path from 'path';
 
 const pckg = jsonfile.readFileSync("./package.json");
+const ang = jsonfile.readFileSync("C:/Source/Repos/Iridium/Modules/ANG/angular.json");
+
 let nextitem: string | null, libraries, project;
 
 // parse command-line arguments.
@@ -33,12 +37,23 @@ process.argv.filter((a, b) => b > 1).forEach(a => {
 
 console.log('Watch NG Libraries CLI utility, (c) 2020');
 
-if (!Object.keys(pckg.devDependencies).includes('@angular/cli')) {
-    console.error('Could not find @angular/cli in package.json');
-} else if ((project || []).length === 0 && (libraries || []).length === 0) {
-    console.error('There is no project and library to compile specified.');
-} else {
-    console.log(`Projects: ${project}`);
-    console.log(`Libraries: ${libraries}`);
-    new ProcessList(libraries || [], project || []);
-};
+// if (!Object.keys(pckg.devDependencies).includes('@angular/cli')) {
+//     console.error('Could not find @angular/cli in package.json');
+// } else if ((project || []).length === 0 && (libraries || []).length === 0) {
+//     console.error('There is no project and library to compile specified.');
+// } else {
+console.log(`Projects: ${project}`);
+console.log(`Libraries: ${libraries}`);
+
+
+getDir(path.join("C:/Source/Repos/Iridium/Modules/ANG/projects/errors/src"));
+
+// new ProcessList(libraries || [], project || []);
+// };
+
+function getDir(dir: string) {
+    let ret;
+    ret = fs.readdirSync(dir, { withFileTypes: true });
+    ret.forEach(f => console.log(f));
+    // ret.filter(s => s.isDirectory).forEach(d => getDir(path.join(dir, d.name)));
+}
