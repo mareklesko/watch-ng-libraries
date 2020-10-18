bf1e9a54a9ee2d413a34459636e7f6f6cf8486ac
Marek Lesko <mlesko100@msn.com>
2020-10-18 19:46:28 +0200

    cleanup

--- lib/processes/process-list.d.ts
+++
@@ -1,4 +0,0 @@
-import { IProcess } from "./process.interface";
-export declare class ProcessList extends Array<IProcess> {
-    constructor(libraries: Array<string>, project: string, path: string, detached: boolean, build: boolean, buildOptions: any, verbose: boolean);
-}
